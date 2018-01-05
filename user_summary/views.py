4479d3dc64aedd977568a2cc6c9f1ba8f7f82d9f
Megha Sharma <meghasharma4910@gmail.com>
2018-01-05 11:15:40 +0530

    Basic Webapp with OAuth Setup

---
+++ user_summary/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    context = {}
+    return render(request, 'user_summary/index.html', context)
+
+@login_required()
+def profile(request):
+    context = {}
+    return render(request, 'user_summary/profile.html', context)
+
+def login_oauth(request):
+    context = {}
+    return render(request, 'user_summary/login.html', context)
