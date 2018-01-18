5078cd99ad7b84f8aa42b34d16f3a3d26bd88f61
Megha Sharma <meghasharma4910@gmail.com>
2018-01-18 20:24:01 +0530

    Cleaning up the repository

--- user_summary/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserSummaryConfig(AppConfig):
-    name = 'user_summary'
--- user_summary/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from user_summary import views
-
-urlpatterns = [
-    url(r'profile', views.profile, name='profile'),
-    url(r'accounts/login', views.login_oauth, name='login'),
-    url(r'oauth/', include('social_django.urls', namespace='social')),
-    url(r'', views.index),
-]
